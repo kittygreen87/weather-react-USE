1b39dd0c9c0c859f6a511c5f1d51bc990245920f
kittygreen87 <catmfreeman@gmail.com>
2022-10-29 22:29:50 -0400

    add axios and weather search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import WeatherSearchbar from "./WeatherSearchbar.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quick Weather Search</h1>
+      <p>
+        Use the search bar below to quickly view current weather for any
+        location.
+      </p>
+      <WeatherSearchbar />
     </div>
   );
 }
