59427060acc9d9bf0b1b1f4f35fb1c0d621cf4a5
kittygreen87 <catmfreeman@gmail.com>
2022-10-30 11:26:39 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-
 import "./App.css";
 import WeatherSearchbar from "./WeatherSearchbar.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>Quick Weather Search</h1>
-      <p>
-        Use the search bar below to quickly view current weather for any
-        location.
-      </p>
-      <WeatherSearchbar />
+      <div className="container">
+        <h1>Quick Weather Search</h1>
+        <p>
+          Use the search bar below to quickly view current weather for any
+          location.
+        </p>
+        <WeatherSearchbar />
+      </div>
     </div>
   );
 }
